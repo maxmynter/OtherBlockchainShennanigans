d0de8d5950d5f79a82c7140afa92c0076924ba83
Max Mynter <maxmynter@me.com>
2025-03-10 22:22:15 -0400

    Fix uint version at 0.9.5

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -6,20 +6,7 @@ construct_uint! {
     pub struct U256(4);
 }
 pub mod crypto;
+pub mod error;
 pub mod sha256;
 pub mod types;
 pub mod util;
-
-impl From<[u8; 32]> for U256 {
-    fn from(bytes: [u8; 32]) -> Self {
-        let mut slice = [0u64; 4];
-        for i in 0..4 {
-            let mut val = 0u64;
-            for j in 0..8 {
-                val = (val << 8) | (bytes[i * 8 + j] as u64)
-            }
-            slice[3 - i] = val;
-        }
-        U256(slice)
-    }
-}
