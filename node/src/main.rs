ad34d13b952c1bb9f457c5f7c3e5d37cb18fa9a0
Max Mynter <maxmynter@me.com>
2025-03-14 21:56:51 -0400

    Add Blockchain fetching from Node

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+mod util;
+
+use anyhow::{Context, Result};
+use argh::FromArgs;
+use btclib::types::Blockchain;
+use dashmap::DashMap;
+use static_init::dynamic;
+use std::path::Path;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::RwLock;
+
+#[dynamic]
+pub static BLOCKCHAIN: RwLock<Blockchain> = RwLock::new(Blockchain::new());
+
+#[dynamic]
+pub static NODES: DashMap<String, TcpStream> = DashMap::new();
+
+#[derive(FromArgs)]
+/// Blockchain node
+struct Args {
+    #[argh(option, default = "9000")]
+    /// port number
+    port: u16,
+
+    #[argh(option, default = "String::from(\"./blockchain.cbor\")")]
+    /// blockchain file location
+    blockchain_file: String,
+
+    #[argh(positional)]
+    nodes: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Args = argh::from_env();
+    let port = args.port;
+    let blockchain_file = args.blockchain_file;
+    let nodes = args.nodes;
+    if Path::new(&blockchain_file).exists() {
+        util::load_blockchain(&blockchain_file).await?;
+    } else {
+        println!("blockchain file does not exist!");
+        util::populate_connections(&nodes).await?;
+        println!("total amount of known nodes: {}", NODES.len());
+        if nodes.is_empty() {
+            println!("no initial nodes provided, starting as a seed")
+        } else {
+            let (longest_name, longest_count) = util::find_longest_chain_node().await?;
+            util::download_blockchain(&longest_name, longest_count).await?;
+            println!("blockchain downloaded from {}", longest_name);
+            {
+                let mut blockchain = BLOCKCHAIN.write().await;
+                blockchain.rebuild_utxos();
+            }
+            {
+                let mut blockchain = BLOCKCHAIN.write().await;
+                blockchain.try_adjust_target();
+            }
+        }
+    }
+    Ok(())
 }
